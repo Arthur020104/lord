fd8998c9392f62f6116165c3802a0966b1a20e9c
Arthur020104 <arthur020104@gmail.com>
2024-05-21 07:50:05 -0300

    Adicionando node base

---
+++ agents/node.py
@@ -0,0 +1,20 @@
+from langchain.chains.llm import LLMChain
+class Node():
+    def __init__(self, llm, children: dict[str, 'Node'], prompt, name):
+        self.llm = llm
+        self.name = name
+        self.children = children
+        self.prompt = prompt
+        self.chain = LLMChain(prompt=self.prompt,llm=llm)
+    def add_child(self, key, value):
+        self.children[key] = value
+    def get_children(self):
+        return self.children
+    def call_chain(self, dict_input:dict):
+        return self.chain.invoke(dict_input)
+    def get_name(self):
+        return self.name
+    def process_input(self,input_last_interaction):
+        pass
+    
+    
