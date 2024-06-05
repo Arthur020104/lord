16555c5910edf6bfac3d1696f5e77d0eace5109c
Arthur020104 <arthur020104@gmail.com>
2024-06-05 13:06:00 -0300

    Mudando a maneira de fazer resquest pra api e adicionando funcao para guardar caminho do audio e texto em um csv

--- tts/text_to_speach.py
+++ tts/text_to_speach.py
@@ -1,34 +1,46 @@
-
-import numpy as np
-import requests
 from datetime import datetime
 from secret.elabapikey import api_key
+import pandas as pd
+import os
+from datetime import datetime
+from elevenlabs import save
+from elevenlabs.client import ElevenLabs
+import threading
+
+# Initialize the ElevenLabs client with your API key
+client = ElevenLabs(api_key=api_key)
+
+def text_to_speech(text):
+    current_time = datetime.now().strftime("%H%M%S")
+    output = f'C:/Users/arthu/OneDrive/Desktop/Repo/LLMProject/Audios/output{current_time}.mp3'
+
+    audio = client.generate(
+        text=text,
+        voice='7u8qsX4HQsSHJ0f8xsQZ', 
+        model='eleven_multilingual_v2',
+        voice_settings={
+            "stability": 0.5,
+            "similarity_boost": 0.75,
+            "style": 0.0,
+            "use_speaker_boost": True
+        }
+    )
+    save(audio, output)
+    
+    thread = threading.Thread(target=save_audio_and_text, args=(output, text))
+    thread.start()
+
+    return output
+
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
-def text_to_speech(text):
-    voice_id = '7u8qsX4HQsSHJ0f8xsQZ'
-    url = f'https://api.elevenlabs.io/v1/text-to-speech/{voice_id}'
-    headers = {
-        'Accept': 'audio/mpeg',
-        'xi-api-key': api_key,
-        'Content-Type': 'application/json'
-    }
-    data = {
-    "text": text,
-    "model_id": "eleven_multilingual_v2",
-    "voice_settings": {
-        "stability": 0.5,
-        "similarity_boost": 0.75,
-        "style": 0.0,
-        "use_speaker_boost": True
-    }}
-    response = requests.post(url, headers=headers, json=data)
-    if response.status_code == 200:
-        current_time = datetime.now().strftime("%H%M%S")
-        with open(f'output{current_time}.mp3', 'wb') as f:
-            f.write(response.content)
-        return f'C:/Users/arthu/OneDrive/Desktop/Repo/LLMProject/output{current_time}.mp3'
+
+def save_audio_and_text(output, text):
+    csv_file = 'Audios/audio_text.csv'
+    if not os.path.exists(csv_file):
+        df = pd.DataFrame(columns=['text', 'file_path'])
     else:
-        print('Error:', response.text)
-    
+        df = pd.read_csv(csv_file)
+    df = pd.concat([df, pd.DataFrame({'text': [text], 'file_path': [output]})], ignore_index=True)
+    df.to_csv(csv_file, index=False)
