eb67e00715509c81468a1f9db5321b99bd055337
Arthur020104 <arthur020104@gmail.com>
2024-06-11 09:24:10 -0300

    arrumando erros

--- agents/node_obj/objection_chain.py
+++ agents/node_obj/objection_chain.py
@@ -2,10 +2,10 @@ from agents.LLM import generate_llm
 from agents.node import Node
 from agents.prompts.node_prompts.objection_prompt import objection_prompt
 
-amenities_chain = Node(
+objection_chain = Node(
     prompt=objection_prompt, 
     llm=generate_llm(temp=0,model=4), 
     children={}, 
-    name="AmenitiesChain", 
+    name="ObjectionChain", 
     property_info_key=['nome','amenidades','construtora','localizacao','data_entrega']
     )
