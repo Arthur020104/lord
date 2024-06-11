a825c52ad0d31e013001c3355c258a008d2addfb
Arthur020104 <arthur020104@gmail.com>
2024-06-11 09:02:38 -0300

    adicionando chaves para o filtro de informacaoes de propriedades

--- agents/node_obj/end_of_conversation_user_no_time.py
+++ agents/node_obj/end_of_conversation_user_no_time.py
@@ -2,4 +2,10 @@ from agents.LLM import generate_llm
 from agents.node import Node
 from agents.prompts.node_prompts.end_of_conversation_user_no_time_prompt import prompt_user_with_no_time
 
-end_of_conversation_user_no_time = Node(llm=generate_llm(temp=0, model=4), prompt=prompt_user_with_no_time, children={}, name="EndOfConversationUserNoTime")
+end_of_conversation_user_no_time = Node(
+    llm=generate_llm(temp=0, model=4), 
+    prompt=prompt_user_with_no_time, 
+    children={}, 
+    name="EndOfConversationUserNoTime",
+    property_info_key=['nome']
+    )
