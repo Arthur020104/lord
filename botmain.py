1a8c17332f97920242f867189277ee2b3ae7acdd
Mekanss <lightkanss@yahoo.com.br>
2024-06-17 11:11:35 -0300

    Adicionei vários comentários

--- botmain.py
+++ botmain.py
@@ -9,13 +9,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from agents.main import call_current_node, process_user_input
 
+# Todo: Descobrir algum jeito de falar com varios ameegans, testar multithreading primeiro, em ultimo caso testar um aws
+# pq nao quero mexer com aws
+
 def setup_whatsapp():
     driver = webdriver.Chrome()  # Utilizando Chrome
     driver.get("https://web.whatsapp.com")
 
-    # Espera o usuário escanear o QR Code
+    # Espera o usuário escanear o QR Code e apertar Enter
     input("Pressione Enter após escanear o QR Code e carregar o WhatsApp Web...")
-    time.sleep(10)
+    time.sleep(5)
 
     return driver
 
@@ -33,7 +36,7 @@ def start_new_conversation(driver, contact_number):
     )
     contact_input.click()
     # Clico nela
-    contact_input.send_keys(contact_number)
+    contact_input.send_keys(contact_number) # Sei la se esses sleeps sao necessários, mas vai que a pagina demora carregar.
     time.sleep(1)
     contact_input.send_keys(Keys.ENTER)
     time.sleep(2)
@@ -54,7 +57,7 @@ def read_last_message(driver):
         
         if all_messages:
             new_message = all_messages[-1]  # Pego a última mensagem
-            print(f"New_Message: {new_message}")
+            print(f"New_Message: {new_message}") 
             print(all_messages)  # Printo todas as mensagens só para ver como funciona...
 
             # Contar quantas vezes a nova mensagem é repetida consecutivamente no final da lista
@@ -66,7 +69,8 @@ def read_last_message(driver):
                     break
             
             if consecutive_count > 0:
-                new_message += '!' * consecutive_count # Pra que serve: Ele checa se a mensagem é igual para ver se uma mensagem
+                new_message += '!' * consecutive_count 
+                # Pra que serve: Ele checa se a mensagem é igual para ver se uma mensagem
                 # nova é adicionada, porém, é possivel que o usuario digite sim, sim, e sejam mensagens diferentes, então se
                 # as mensagens forem consecutivas e no final eu vou adicionando ! para diferenciar elas, é gambiarra mas funciona ok!
             return new_message
@@ -122,46 +126,47 @@ def save_conversation(user_input, response, phone_number, data_resposta_user):
 
 
 # O que isso faz: A função main_loop() roda até ctrl+c ser ativado ou um erro acontecer, ela entra no whatsapp_web
-# espera a confirmação do QR code, e então você tem que dar enter no console, ela vai digitar o nome do contact
+# espera a confirmação do QR code, e então você tem que dar enter no console, ela vai digitar o numero do contact
 # na caixa de busca e então apertar enter, ela faz uma chamada para a LLM, e manda a resposta inicial, então
 # coloca as mensagens em uma lista, e pega apenas a ultima para mandar para a LLM e ir resolvendo essa bomba.
 
 def main_loop(phone_number):
     driver = setup_whatsapp()
-    start_new_conversation(driver, phone_number)  # Adicione o número de telefone aqui
+    start_new_conversation(driver, phone_number)
 
-    # Inicializa a última mensagem com a mensagem mais recente da conversa
+    # Inicializa a última mensagem com a mensagem mais recente da conversa, essa bomba aqui é pra não pegarmos mensagens
+    # antigas...
     last_message = read_last_message(driver)
     print(f"Última mensagem inicializada como: {last_message}") # Printando aqui so pra deixar claro o que ta rolando
 
-    response = call_current_node()['text']
-    send_message(driver, response)
+    response = call_current_node()['text'] # Transformando o dicionario em string
+    send_message(driver, response) # Mandando a primeira mensagem da LLM, da startconversationchain...
 
     try:
         while True:
             # Ler a última mensagem
             user_input = read_last_message(driver)
-            if user_input and user_input != last_message: # Isso daqui eu preciso mudar, ja que o cliente pode responder Sim,
-                # sim e ainda seria uma mensagem nova.
+            if user_input and user_input != last_message: 
 
                 # Vendo o input que vem da messagem
                 print(f"O input para ser processado foi: {user_input}")
                 data_resposta_user = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # Hora que usuario respondeu
-                last_message = user_input
-                process_user_input(user_input)
+                last_message = user_input # last_message vai pegar o valor de user input pra comparar se existem mensagens
+                # novas
+                process_user_input(user_input) # Manda pra LLM
 
                 # Chama a resposta denovo
-                response = call_current_node()['text']
-                send_message(driver, response)
+                response = call_current_node()['text'] # Pega da LLM
+                send_message(driver, response) # Envia pro zipzoper
 
                 # Salva a conversa no arquivo JSON
-                save_conversation(user_input, response, phone_number, data_resposta_user)
+                save_conversation(user_input, response, phone_number, data_resposta_user) # Salvando no Json
             
             time.sleep(1)  # Aguarda 1 segundo antes de verificar novamente
     except KeyboardInterrupt:
         print("Chat interaction stopped.") # Usa Ctrl + C pra cancelar interação do bot
     finally:
-        driver.quit()
+        driver.quit() # Tenho menor ideia do que isso faz
 
 if __name__ == "__main__":
     main_loop(phone_number = "553492631397") # Setando o numero de telefone
