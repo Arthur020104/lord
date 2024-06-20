b56441bc99208c32520830622b204a0deacbdf90
Mekanss <lightkanss@yahoo.com.br>
2024-06-20 08:47:23 -0300

    Documentei essa bomba, mas tem algumas coisas confusas ainda, depois o arthur tem que olhar

--- AgentBuild/Agent/Agent.py
+++ AgentBuild/Agent/Agent.py
@@ -4,15 +4,15 @@ from langchain_core.runnables.base import RunnableSequence
 import json
 class Agent:
     def __init__(self,initial_node:Node, all_nodes:dict, memory:CustomConversationTokenBufferMemory,agent_info:dict, manager:RunnableSequence, nodes_info:dict):
-        self.node = initial_node
-        self.initial_node = initial_node
-        self.all_nodes = all_nodes
-        self.memory = memory
-        self.agent_info = agent_info
-        self.manager = manager
-        self.last_output = ""
-        self.nodes_info = nodes_info
-        # Add children to nodes
+        self.node = initial_node # Ponto de partida / No inicial
+        self.initial_node = initial_node  # ??? Acho que isso é redundante
+        self.all_nodes = all_nodes # Todos os nós que o agente pode suar
+        self.memory = memory # Guarda memorias de conversas passadas
+        self.agent_info = agent_info # Informações do agente / Nao sei exatamente o que é isso
+        self.manager = manager # Manager, ou router que vai gerir e guiar os nós.
+        self.last_output = "" # Ultima resposta 
+        self.nodes_info = nodes_info # Informação especifica de cada nó
+        # Fazendo todo mundo se ligar
         for node_name, cnode in self.all_nodes.items():
             for child_name, child_node in all_nodes.items():
                 cnode.add_child(child_name, child_node)
@@ -20,11 +20,11 @@ class Agent:
 
     def call_current_node(self):
         try:
-            print(f"Current node: {self.node.get_name()}")
-            self.nodes_info['chat_history'] = self.memory.get_memory_tuple()
-            self.nodes_info['property_info'] = self.node.filter_property_info(self.agent_info)
-            response = self.node.call_chain(self.nodes_info)
-            self.last_output = response['text']
+            print(f"Current node: {self.node.get_name()}") # Mostra o nó atual
+            self.nodes_info['chat_history'] = self.memory.get_memory_tuple() # Atualiza historico
+            self.nodes_info['property_info'] = self.node.filter_property_info(self.agent_info) # Filtra as informações do property_info
+            response = self.node.call_chain(self.nodes_info) # Chama a funcao de resposta do nó atual
+            self.last_output = response['text'] # Transforma isso em texto e retorna
             return response
         except Exception as e:
             print(f"Error in call_current_node: {e}")
@@ -32,22 +32,26 @@ class Agent:
 
     def process_user_input(self,user_input):
         try:
+            # Pega ultimo input do usuario e a ultima resposta que está na memoria
             self.memory.add(human_input=user_input, ia_output=self.last_output)
+            # Processa o input no nó atual
             self.node.process_input(self.memory.get_memory()[-1])
             
-            self.nodes_info['chat_history'] = self.memory.get_memory_tuple()
-            for _ in range(3):
+            self.nodes_info['chat_history'] = self.memory.get_memory_tuple() # Atualiza o histórico de chat
+
+            for _ in range(3): # Tenta no máximo 3 vezes, nao sei pq isso ta aqui mas tá
                 try:
+                    # tenta chamar o próximo nó que o manager decidir
                     node_called = self.manager.invoke({
                         'chat_history': self.memory.get_memory_tuple(),
                         'nodes': list(self.node.get_children().keys()),
                         'current_node': self.node.get_name()
                     })
                     data_dict = node_called
-                    node_called = data_dict['node']
-                    print(f"Node called: {node_called}, why called: {data_dict['answer']}")
-                    if node_called != "Não existe":
-                        self.node = self.node.get_children()[node_called]
+                    node_called = data_dict['node'] # proximo nó chamado
+                    print(f"Node called: {node_called}, why called: {data_dict['answer']}") # Por que chamou o proximo nó
+                    if node_called != "Não existe": # Se achou nó valido
+                        self.node = self.node.get_children()[node_called] # muda para o nó válido
                     break
                 except json.JSONDecodeError as e:
                     print(f"JSON decode error: {e}")
@@ -56,8 +60,8 @@ class Agent:
         except Exception as e:
             print(f"Error in process_user_input: {e}")
 
-    def delete_memory(self):
-        self.memory.delete_memory()
+    def delete_memory(self): # Apaga as memorias e volta para nó inicial
+        self.memory.delete_memory() 
         self.node = self.initial_node
         print("Memory deleted and conversation reset to initial node.")
 
