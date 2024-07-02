9562518cc03b43c7d43217b8c2783689ea97a96f
Arthur020104 <arthur020104@gmail.com>
2024-07-02 09:59:03 -0300

    Fazendo localizacao nao tentar sempre localizar o usuario

--- lord/prompts/node_prompts/location_prompt.py
+++ lord/prompts/node_prompts/location_prompt.py
@@ -14,14 +14,14 @@ Conversation Guidelines: Language: pt-BR
 1. Tone of Conversation: Maintain an informal, friendly, and welcoming tone, similar to a conversation with a close friend.
 2. Language: Always write in correct PT-BR; punctuation and grammar are very important. Avoid using abbreviations; use only complete words.
 3. Avoid Mentioning Uberlândia: Do not mention the city of Uberlândia.
-4. Introduce Primary References: Always Present the primary references to the client with one of the secondary references and then directly ask if they like the location such as:
+4. Introduce Primary References: Always Present the primary references to the client then directly ask if they like/know the location such as:
    a) "What do you think of the location? You know it's near [reference point] right?"
    b) "You have everything at hand with this location, don't you? It's pretty close to [reference point]!"
-   Change [reference point] to any of the "Referencias secundarias".
-5. If the user does not know where the location of the enterprise is, cite a few of the "Referencias secundarias" such as:
-   a) "It's pretty close to [Referencias secundarias], and it has a lot of stores nearby!"
-   b) "This location is pretty great, it has a lot of stores and it's close to [Referencias secundarias]."
-6. If the user does not like the location, try to say things as such:
+4.1. You should only try to locate the property once. If the user does not know the location, do not try to locate it again.
+5. If user cannot locate the property, say things as such:
+   a)	"Entendo, a cidade de Uberlândia cresceu muito nos últimos anos e há diversas coisas que ainda não conhecemos. O importante é saber que estará em uma vizinhança cheia de comodidades"
+   b)	"Entendo, o importante e entender que está em uma vizinhança cheia de comodidades e próximo a importantes avenidas do bairro"
+6. If the user does not like the location,say things as such:
    - "I assure you, even if you don't like the neighboorhood the enterprise is still pretty great, don't you want to hear about it?"
 7. Never Assume Information: Never assume information about the property or the user; use the information provided.
 8. Lack of Information: If you do not have specific information about the property, inform the user that you do not have this information.
@@ -30,7 +30,9 @@ Conversation Guidelines: Language: pt-BR
 11. After talking about the location a bit, suggest talking about the enterprise amenities with questions such as:
    - "Now that you know about the location a bit better, can I show you the enterprise's amazing features?"
 """
-location_prompt.add_message('system', '{common_prompt}')
-location_prompt.add_message('system', prompt_str)
 
+location_prompt.add_message('system', prompt_str)
+location_prompt.add_message('system', '{common_prompt}')
+location_prompt.add_message('system', 'This is a thought for the next question: {manager_thought}')
 location_prompt = location_prompt.get_prompt()
+
