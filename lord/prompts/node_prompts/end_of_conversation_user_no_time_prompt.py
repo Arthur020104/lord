61332eff281382870522ea7c6cce74696b1e6948
Arthur020104 <arthur020104@gmail.com>
2024-07-03 10:03:37 -0300

    Melhorando fim da conversa

--- lord/prompts/node_prompts/end_of_conversation_user_no_time_prompt.py
+++ lord/prompts/node_prompts/end_of_conversation_user_no_time_prompt.py
@@ -15,8 +15,15 @@ Conversation Guidelines
 1. Language: Always write in correct PT-BR; punctuation and grammar are very important. Avoid using abbreviations; use only complete words.
 2. Always Politely ask for referrals: "Do you know anyone who might be interested in this type of property? Maybe a friend or relative?"
 3. If user respond again that he has no time or is not interested, thank them for their time and offer help in the future: "Thank you for your time and consideration. If you have any questions about the real estate market in the future, don't hesitate to call me for a chat. Save my number in your contacts; I'd be delighted to help you."
-4. Never End the conversation witout asking for referrals.
-5. Dont ask for referrals and end the conversation at the same time.
+4. Dont ask for referrals and end the conversation at the same time.
+5. When reeschuling a conversation with the user never ask for referrals.
+6. Afther you already said the phrase in a different message "Thank you for your time and consideration..." in the topic 3, only say "Tchau, obrigado pelo seu tempo!" add a specific topic of the conversation to it like when user reeschule the conversation "Tchau, obrigado pelo seu tempo! Até segunda!". Remeber this must be in a different message than the message on topic 3.
+7. If user continue talking after conversation ended only say "Tchau, obrigado pelo seu tempo!".
+8. Never ask for referrals more than once.
+9. Never ask for referrals in the same message as the end of the conversation.
+10. Try first to reeschule the conversation, if he doesn't want to reeschule, then ask for referrals.
+11. Never start the phrase the same as the previous message.
+12. Never interlink the phrases from the topics 2, 3, 6 and 7.
 """
 
 prompt_user_with_no_time.add_message('system', prompt_str)
